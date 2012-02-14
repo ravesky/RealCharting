dacb2e08a3eb63602c6aece2653670847c67476b
Angel Martin <perezagua@gmail.com>
2012-02-14 17:50:37 +0100

    Only one axis in line chart graph

--- client/js/chart.js
+++ client/js/chart.js
@@ -2,25 +2,28 @@ var jWebSocketClient = null;
 
 var pieChart;
 var lineChart;
-	
-var lineChartData = [];
+
+var lineChartData = [
+			{date:new Date(2012,1,14,12,12,12),"OPEN":33,"CLOSED":10,"INIT":30},
+			{date:new Date(2012,1,14,12,12,13),"OPEN":20,"CLOSED":20,"INIT":20},
+			{date:new Date(2012,1,14,12,12,14),"OPEN":40,"CLOSED":36,"INIT":60},
+			{date:new Date(2012,1,14,12,12,15),"OPEN":50,"CLOSED":26,"INIT":56},
+			{date:new Date(2012,1,14,12,12,16),"OPEN":20,"CLOSED":21,"INIT":33},
+			{date:new Date(2012,1,14,12,12,17),"OPEN":4,"CLOSED":23,"INIT":13}];
 
 
 function parsePieChartData(stream) {
 	
 	var streamData = jQuery.parseJSON(stream);
-	var pieDataProvider = [];
-
-	$.each(streamData.query, function(intIndex, objValue) {
-		var status = objValue.status;
-		var count = objValue.count;
-
-		var pieObject = {status:status, count:count};
-		pieDataProvider.push(pieObject);
-	});
- 
+	
+	var pieDataProvider = [
+		{status:"OPEN", count:streamData.OPEN},
+		{status:"CLOSED", count:streamData.CLOSED},
+		{status:"INIT", count:streamData.INIT}
+	];
+	
+ 	pieChart.colors = ["#FF0F00", "#FF6600", "#FF9E01"];
 	pieChart.dataProvider = pieDataProvider;
-	pieChart.colors = ["#FF0F00", "#FF6600", "#FF9E01"];
 	pieChart.validateData();
 }
 
@@ -28,26 +31,20 @@ function parsePieChartData(stream) {
 function parseLineChartData(stream) {
 	
 	var streamData = jQuery.parseJSON(stream);
-	var temp = [];
 	
 	if (lineChartData.length > 5) {
 		lineChartData.splice(0,1);
 	}
-
+			
 	lineChartData.push(
 		{
-		 date:new Date(), 
-		 OPEN:Math.round(Math.random() * 40) + 100, 
-		 CLOSED:Math.round(Math.random() * 80) + 500,
-		 INIT:Math.round(Math.random() * 6000)
+		 date:new Date(),
+		 OPEN:streamData.OPEN, 
+		 CLOSED:streamData.CLOSED,
+		 INIT:streamData.INIT
 		}
 	);
 	
-	temp = lineChartData;
-				$('#log').html("");
-			$('#log').html("temp " + temp.length);		
-			
-	lineChart.dataProvider = temp;
 	lineChart.validateData();
 	
 } // function parseLineChartData(stream)
@@ -93,6 +90,8 @@ $(document).ready(function() {
 		***********************************/
 		lineChart = new AmCharts.AmSerialChart();
       lineChart.dataProvider = lineChartData;
+		lineChart.marginRight = 5;
+		lineChart.marginLeft = 5;
       lineChart.categoryField = "date";
 		 
 		
@@ -103,56 +102,55 @@ $(document).ready(function() {
 		categoryAxis.gridAlpha = 0.15;
 		categoryAxis.axisColor = "#DADADA";
 		categoryAxis.dateFormats = [{period: "ss",format: "JJ:NN:SS"}];
-		/*
-		,
-			{period: "DD",format: "DD"},
-			{period: "WW",format: "MMM DD"},
-			{period: "MM",format: "MMM"},
-			{period: "YYYY",format: "YYYY"}
-		];
-		*/             
 
-      var valueAxis1 = new AmCharts.ValueAxis();
-      valueAxis1.gridAlpha = 0;
-      valueAxis1.axisColor = "#FF6600";
-      valueAxis1.axisThickness = 2;
-		valueAxis1.logarithmic = true;
-      lineChart.addValueAxis(valueAxis1);
+      var valueAxis = new AmCharts.ValueAxis();
+      valueAxis.gridAlpha = 0;
+      valueAxis.axisColor = "#DADADA";
+      valueAxis.axisThickness = 2;
+		//valueAxis.logarithmic = true;
+		valueAxis.inside = true;
+      lineChart.addValueAxis(valueAxis);
 
-      var valueAxis2 = new AmCharts.ValueAxis();
-      valueAxis2.gridAlpha = 0;
-      valueAxis2.axisColor = "#FCD202";
-      valueAxis2.axisThickness = 2;
-		valueAxis2.position = "right"
-		valueAxis2.logarithmic = true;
-      lineChart.addValueAxis(valueAxis2);
-
-      var valueAxis3 = new AmCharts.ValueAxis();
-      valueAxis3.gridAlpha = 0;
-      valueAxis3.axisColor = "#B0DE09";
-      valueAxis3.axisThickness = 2;
-		valueAxis3.offset = 50;
-      lineChart.addValueAxis(valueAxis3);
-		
 		var graph1 = new AmCharts.AmGraph();
-      graph1.valueAxis = valueAxis1;
+      graph1.valueAxis = valueAxis;
+		graph1.type = "smoothedLine";
+      graph1.bullet = "round";
+      graph1.bulletColor = "#FFFFFF";
+      graph1.bulletBorderColor = "#FF0F00";
+      graph1.bulletBorderThickness = 2;
+      graph1.bulletSize = 5;
       graph1.title = "Open";
       graph1.valueField = "OPEN";
-      graph1.hideBulletsCount = 30;
+		graph1.lineColor = "#FF0F00";
+		graph1.lineThickness = 2;
       lineChart.addGraph(graph1);
 
       var graph2 = new AmCharts.AmGraph();
-      graph2.valueAxis = valueAxis2;
+      graph2.valueAxis = valueAxis;
+		graph2.type = "smoothedLine";
+      graph2.bullet = "round";
+      graph2.bulletColor = "#FFFFFF";
+      graph2.bulletBorderColor = "#FF6600";
+      graph2.bulletBorderThickness = 2;
+      graph2.bulletSize = 5;		
       graph2.title = "Closed";
       graph2.valueField = "CLOSED";
-      graph2.hideBulletsCount = 30;
+		graph2.lineColor = "#FF6600";
+		graph2.lineThickness = 2;
       lineChart.addGraph(graph2);
 
       var graph3 = new AmCharts.AmGraph();
-      graph3.valueAxis = valueAxis3;
+      graph3.valueAxis = valueAxis;
+		graph3.type = "smoothedLine";
+      graph3.bullet = "round";
+      graph3.bulletColor = "#FFFFFF";
+      graph3.bulletBorderColor = "#FF9E01";
+      graph3.bulletBorderThickness = 2;
+      graph3.bulletSize = 5;		
       graph3.title = "Init";
       graph3.valueField = "INIT";
-      graph3.hideBulletsCount = 30;
+		graph3.lineColor = "#FF9E01";
+		graph3.lineThickness = 2;
       lineChart.addGraph(graph3);
 
 		lineChart.write("lineChartDiv");
@@ -181,13 +179,13 @@ $(document).ready(function() {
 					result = jWebSocketClient.registerStream(stream);
 					//alert("Result Register: " + jWebSocketClient.resultToString(result));
     			},  
-				OnMessage: function(levent) {
+				OnMessage: function(levent, token) {
 					if(jWebSocketClient.isLoggedIn()) {
 						parsePieChartData(levent.data);
 						parseLineChartData(levent.data);
 					} else {
-						$('#log').html("");
-						$('#log').html("conected");		
+						//$('#log').html("");
+						//$('#log').html("conected");		
 					}
 				},
 				OnClose: function(levent) {
