77c98654577a612cdb2f3301f6872b03e3155268
Angel Martin <perezagua@gmail.com>
2012-02-28 23:22:48 +0100

    Clean client side

--- server/jWebSocketStreamingPlugIn/src/main/java/org/jwebsocket/plugins/streaming/ChartStream.java
+++ server/jWebSocketStreamingPlugIn/src/main/java/org/jwebsocket/plugins/streaming/ChartStream.java
@@ -61,7 +61,7 @@ public class ChartStream extends TokenStream {
       try {
           Class.forName("com.mysql.jdbc.Driver");
           mConnection = DriverManager.getConnection(
-                  "jdbc:mysql://16.38.0.15/jWebSocket", "root", "toor");
+                  "jdbc:mysql://localhost/jWebSocket", "root", "");
           
          if (log.isDebugEnabled()) {
                log.debug("Connection with database established...");
