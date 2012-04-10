ac9fe0aa1dd82f57dd6388bb4b8ac5dda98138a1
Angel Martin <perezagua@gmail.com>
2012-04-10 23:53:22 +0200

    Included pie chart

--- client/js/chart.js
+++ client/js/chart.js
@@ -1,134 +1,23 @@
 jQuery(function($) {
-	
+
 	var jWebSocketClient = null;
 	var lineChartData = [];
-	var pieChartData = [];
+	var	pieChartData = [{airline:"NONE", passengers:1}];
 	
-	function parseChartData(stream) {
-     
-	  	var streamData = jQuery.parseJSON(stream);
-			
-			console.log(streamData);
-			
-     	if (lineChartData.length > 5) {
-				lineChartData.splice(0,1);
-     	}
-		
-	  	$('#cnt-landing').text(streamData.CNT_LANDING);
- 	  	$('#cnt-boarding').text(streamData.CNT_BOARDING);
- 	  	$('#cnt-taking-off').text(streamData.CNT_TAKING_OFF);
+	var pieChart = new AmCharts.AmPieChart();
+  pieChart.dataProvider = pieChartData;
+  pieChart.titleField = "airline";
+  pieChart.valueField = "passengers";
+  pieChart.outlineColor = "#FFFFFF";
+  pieChart.outlineAlpha = 0.8;
+  pieChart.outlineThickness = 2;
+	pieChart.labelRadius = -30;
+  pieChart.labelText = "[[percents]]%";
+	pieChart.dataChanged = true;
+	pieChart.radius = 115;
+	pieChart.startDuration = 0;
 
-	  	$('#ave-landing').text(streamData.AVE_LANDING.toFixed(2));
- 	  	$('#ave-boarding').text(streamData.AVE_BOARDING.toFixed(2));
- 	  	$('#ave-taking-off').text(streamData.AVE_TAKING_OFF.toFixed(2));
-			
-			if (streamData.CNT_LANDING > parseInt($('#max-landing').text())) {
-				$('#max-landing').text(streamData.CNT_LANDING);
-			}
-			
-			if (streamData.CNT_BOARDING > parseInt($('#max-boarding').text())) {
-				$('#max-boarding').text(streamData.CNT_BOARDING);
-			}
-			
-  		if (streamData.CNT_TAKING_OFF > parseInt($('#max-taking-off').text())) {
-				$('#max-taking-off').text(streamData.CNT_TAKING_OFF);
-			}
-		
-     	lineChartData.push(
-	  		{DATE:new Date(),
-				 LANDING:streamData.CNT_LANDING,
-				 BOARDING:streamData.CNT_BOARDING,
-				 TAKING_OFF:streamData.CNT_TAKING_OFF});
-
-			lineChart.validateData();
-			
-			pieChartData = streamData.list;
-			
-			pieChart.validateData();
-			
-			/*
-			pieChartData = [
-				{"airline":"Air France","passengers":268},
-				{"airline":"Lufthansa","passengers":419},
-				{"airline":"Iberia","passengers":219},
-				{"airline":"British Airways","passengers":450},
-				{"airline":"Aerolíneas Argentinas","passengers":245},
-				{"airline":"American Airlines","passengers":291},
-				{"airline":"Qantas","passengers":1220}
-			];
-			*/
-	}
-
-	function disconnect() {
-		
-		if(jWebSocketClient) {
-			jWebSocketClient.stopKeepAlive();
-			var result = jWebSocketClient.close();
-			
-			if (result.code == 0) {
-				$('#alert-div').html('').removeClass().addClass("alert alert-info");
-				$('#alert-div').html('<strong>Info!</strong> jWebSocket disconnected');
-			} else {
-				$('#alert-div').html('').removeClass().addClass("alert alert-error");
-				$('#alert-div').html('<strong>Info!</strong> Error while disconnecting');
-			}
-		}
-	}
-
-	function registerStream() {
-		var stream = "chartStream";
-		var result = jWebSocketClient.registerStream(stream);
-		result = jWebSocketClient.resultToString(result);
-		$('#alert-div').html('').removeClass().addClass("alert alert-success");
-		$('#alert-div').html('<strong>Info!</strong> jWebSocket connection established " + result');
-	}
-
-	
-	/* Main Code */
-	
-	if(jws.browserSupportsWebSockets()) {
-  		jWebSocketClient = new jws.jWebSocketJSONClient();
-  	} else {
-			$('#alert-div').html('').removeClass().addClass("alert alert-error");
-			$('#alert-div').html('<strong>Error!</strong> ' + jws.MSG_WS_NOT_SUPPORTED);			
-	}
-	
-	$('#connect').toggle(function() {
-		var URL = jws.getDefaultServerURL();
-	
-		try {
-			var result = jWebSocketClient.logon(URL, jws.GUEST_USER_LOGINNAME, jws.GUEST_USER_PASSWORD, {
-				OnOpen: function(levent) {
-				},
-			  OnWelcome: function(levent) {
-					registerStream();
-    		},  
-				OnMessage: function(levent, token) {
-					if(jWebSocketClient.isLoggedIn()) {
-						parseChartData(levent.data);
-						$('#alert-div').html('').removeClass().addClass("alert alert-info");
-						$('#alert-div').html('<strong>Info!</strong> ' + levent.data);
-					} else {
-						$('#alert-div').html('').removeClass().addClass("alert alert-info");
-						$('#alert-div').html('<strong>Info!</strong> Authentication process...');
-					}
-				},
-				OnClose: function(levent) {
-					$('#alert-div').html('').removeClass().addClass("alert alert-info");
-					$('#alert-div').html('<strong>Info!</strong> jWebSocket connection closed.');
-				}
-			}); // jWebSocketClient.logon
-		} catch(e) {
-			logger("Exception: " + e.message );
-		}
-		
-		$('#connect').text('Disconnect');
-	}, 
-	function() {
-		disconnect();
-		$('#connect').text('Connect');
-	});
-	
+  pieChart.write("pieChartDiv");
 	
 	var lineChart = new AmCharts.AmSerialChart();
 	lineChart.dataProvider = lineChartData;
@@ -141,7 +30,7 @@ jQuery(function($) {
 	var categoryAxis = lineChart.categoryAxis;
   categoryAxis.parseDates = true;
   categoryAxis.minPeriod = "ss";
-  categoryAxis.dashLength = 12;
+  categoryAxis.dashLength = 5;
   categoryAxis.gridAlpha = 0.15;
   categoryAxis.axisColor = "#DADADA";
   categoryAxis.dateFormats = [{period: "ss",format: "JJ:NN:SS"}, {period: "mm",format: "JJ:NN:SS"}];
@@ -195,24 +84,125 @@ jQuery(function($) {
 	takingOffGraph.lineThickness = 2;
 	lineChart.addGraph(takingOffGraph);
 
-	lineChart.write("line-chart-div");
+	lineChart.write("lineChartDiv");
 	
-	
-  var pieChart = new AmCharts.AmPieChart();
-  pieChart.dataProvider = pieChartData;
-  pieChart.titleField = "airline";
-  pieChart.valueField = "passengers";
-  pieChart.outlineColor = "#FFFFFF";
-  pieChart.outlineAlpha = 0.8;
-  pieChart.outlineThickness = 2;
-	pieChart.labelRadius = -30;
-  pieChart.labelText = "[[percents]]%";
-	pieChart.dataChanged = true; 
+	function parseChartData(stream) {
+		
+	  	var streamData = jQuery.parseJSON(stream);
+			
+			pieChartData.splice(0,streamData.list.length);
+			
+			$.each(streamData.list, function(index,record) {
+				pieChartData.push({"airline":record.airline, "passengers":record.passengers});				
+			});
+		
+			pieChart.validateData();
+			
+     	if (lineChartData.length > 5) {
+				lineChartData.splice(0,1);
+     	}
+		
+	  	$('#cnt-landing').text(streamData.CNT_LANDING);
+ 	  	$('#cnt-boarding').text(streamData.CNT_BOARDING);
+ 	  	$('#cnt-taking-off').text(streamData.CNT_TAKING_OFF);
 
-  pieChart.write("pie-chart-div");
+	  	$('#ave-landing').text(streamData.AVE_LANDING);
+ 	  	$('#ave-boarding').text(streamData.AVE_BOARDING);
+ 	  	$('#ave-taking-off').text(streamData.AVE_TAKING_OFF);
+			
+			if (streamData.CNT_LANDING > parseInt($('#max-landing').text())) {
+				$('#max-landing').text(streamData.CNT_LANDING);
+			}
+			
+			if (streamData.CNT_BOARDING > parseInt($('#max-boarding').text())) {
+				$('#max-boarding').text(streamData.CNT_BOARDING);
+			}
+			
+  		if (streamData.CNT_TAKING_OFF > parseInt($('#max-taking-off').text())) {
+				$('#max-taking-off').text(streamData.CNT_TAKING_OFF);
+			}
+		
+     	lineChartData.push(
+	  		{DATE:new Date(),
+				 LANDING:streamData.CNT_LANDING,
+				 BOARDING:streamData.CNT_BOARDING,
+				 TAKING_OFF:streamData.CNT_TAKING_OFF});
+				 
+			lineChart.validateData();
+
+	}
+
+	function disconnect() {
+		
+		if(jWebSocketClient) {
+			jWebSocketClient.stopKeepAlive();
+			var result = jWebSocketClient.close();
+			
+			if (result.code == 0) {
+				$('#alert-div').html('').removeClass().addClass("alert alert-info");
+				$('#alert-div').html('<strong>Info!</strong> jWebSocket disconnected');
+			} else {
+				$('#alert-div').html('').removeClass().addClass("alert alert-error");
+				$('#alert-div').html('<strong>Info!</strong> Error while disconnecting');
+			}
+		}
+	}
+
+	function registerStream() {
+		var stream = "chartStream";
+		var result = jWebSocketClient.registerStream(stream);
+		result = jWebSocketClient.resultToString(result);
+		$('#alert-div').html('').removeClass().addClass("alert alert-success");
+		$('#alert-div').html('<strong>Info!</strong> jWebSocket connection established " + result');
+	}
+
+	
+	/* Main Code */
+	
+	if(jws.browserSupportsWebSockets()) {
+  		jWebSocketClient = new jws.jWebSocketJSONClient();
+  	} else {
+			$('#alert-div').html('').removeClass().addClass("alert alert-error");
+			$('#alert-div').html('<strong>Error!</strong> ' + jws.MSG_WS_NOT_SUPPORTED);			
+	}
+	
+	$('#connect').toggle(function() {
+		var URL = jws.getDefaultServerURL();
+
+		try {
+			var result = jWebSocketClient.logon(URL, jws.GUEST_USER_LOGINNAME, jws.GUEST_USER_PASSWORD, {
+				OnOpen: function(levent) {
+				},
+			  OnWelcome: function(levent) {
+					registerStream();
+    		},  
+				OnMessage: function(levent, token) {
+					if(jWebSocketClient.isLoggedIn()) {
+						parseChartData(levent.data);
+						$('#alert-div').html('').removeClass().addClass("alert alert-info");
+						$('#alert-div').html('<strong>Info!</strong> ' + levent.data);
+					} else {
+						$('#alert-div').html('').removeClass().addClass("alert alert-info");
+						$('#alert-div').html('<strong>Info!</strong> Authentication process...');
+					}
+				},
+				OnClose: function(levent) {
+					$('#alert-div').html('').removeClass().addClass("alert alert-info");
+					$('#alert-div').html('<strong>Info!</strong> jWebSocket connection closed.');
+				}
+			}); // jWebSocketClient.logon
+		} catch(e) {
+			console.log("Exception: " + e.message );
+		}
+		
+		$('#connect').text('Disconnect');
+	}, 
+	function() {
+		disconnect();
+		$('#connect').text('Connect');
+	});
 	
 }); // jQuery(function($)
-
 
 $(document).unload(function() {
 	jWebSocketClient.stopKeepAlive();
